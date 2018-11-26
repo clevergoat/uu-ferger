501f93fabe0bd1718efe2adabfb373a492f9df82
Rikard Andresen <clevergoat@MacBook-Pro-2.local>
2018-11-26 09:54:25 +0100

    Home template and .container widths on all templates

--- archive.php
+++ archive.php
@@ -9,7 +9,7 @@ get_header(); ?>
 			
 	<div class="content">
 	
-		<div class="inner-content grid-x grid-margin-x grid-padding-x">
+		<div class="inner-content container grid-x grid-margin-x grid-padding-x">
 		
 		    <main class="main small-12 medium-8 large-8 cell" role="main">
 			    
---
+++ template-full-width.1.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+Template Name: Hjem
+*/
+
+get_header(); ?>
+	
+	<div class="content">
+	
+		<div class="inner-content container grid-x grid-margin-x grid-padding-x">
+	
+		    <main id="main-sticky" class="main small-12 large-8 medium-8 cell" role="main">
+				
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
+			    
+			    <?php endwhile; endif; ?>							
+			    					
+			</main> <!-- end #main -->
+
+		    <?php get_sidebar(); ?>
+		    
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
